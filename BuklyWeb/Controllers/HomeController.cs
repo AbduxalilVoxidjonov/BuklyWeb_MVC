93447f3a472e9060b30e89a95f2b97335cd77d92
abduh <abduh@LENOVO>
2026-04-22 15:07:38 +0500

    Add project files.

---
+++ BuklyWeb/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using BuklyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BuklyWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
